dbe1fcd0db9e00ffca1748a077c876c31bde8f44
Amr El-Tahhan <amr.wf49@gmail.com>
2020-06-25 03:23:04 +0200

    fixed images don't load issue

--- week4/Filmer/app/src/main/java/com/tahhan/filmer/Constants.java
+++ week4/Filmer/app/src/main/java/com/tahhan/filmer/Constants.java
@@ -1,15 +1,13 @@
 package com.tahhan.filmer;
 
 public class Constants {
-    public final static String SORT_POPULAR = "popularity.desc";
-    public final static String SORT_HIGHEST_RATED = "vote_average.desc";
+   
 
-    //TODO INSERT APIKEY
     public final static String MOVIEDB_APIKEY = "fc2325576147689162c365169ca6119d";
 
     public final static String BASE_API_URL = "https://api.themoviedb.org/3/";
-    public final static String ROOT_POSTER_IMAGE_URL = "http://image.tmdb.org/t/p/w185";
-    public final static String ROOT_BACKDROP_IMAGE_URL = "http://image.tmdb.org/t/p/w500";
+    public final static String ROOT_POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w185";
+    public final static String ROOT_BACKDROP_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
     public final static String baseVideo = "https://www.youtube.com/watch?v=";
 
 }
